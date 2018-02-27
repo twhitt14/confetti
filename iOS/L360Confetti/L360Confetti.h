6b621c4e44c0defb98acd225399314883917e6c3
Maksym Grebenets <maksym.grebenets@cba.com.au>
2018-02-27 16:53:02 +1100

    Add missing umbrella header
    - Umbrella header is important for tools like buck
    - It's also the default header used in auto-generated header maps

---
+++ iOS/L360Confetti/L360Confetti.h
@@ -0,0 +1,15 @@
+//
+//  L360Confetti.h
+//  L360ConfettiExample
+//
+//  Created by Joshua Archer on 2/27/18.
+//  Copyright © 2018 Life360. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+
+#import <L360Confetti/L360ConfettiAble.h>
+#import <L360Confetti/L360ConfettiArea.h>
+#import <L360Confetti/L360ConfettiObject.h>
+#import <L360Confetti/L360ConfettiView.h>
